f4db1527d4070dfec5dcb0b266e7518357ba5b3c
boyvic <117716643@qq.com>
2022-04-11 09:15:17 +0000

    add module

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/boyvic/hellogo
+
+go 1.14
--- main.go
+++ main.go
@@ -4,5 +4,8 @@ import "fmt"
 
 func main() {
 	// v1.0.0 未引用任何第三方包，也未使用go module
-	fmt.Println("v1.0.0 未引用任何第三方包，也未使用go module")
+	// fmt.Println("v1.0.0 未引用任何第三方包，也未使用go module")
+
+	// v1.1.0 未引用任何第三方包，已开始使用go module，但没有任何外部依赖
+	fmt.Println("v1.1.0 未引用任何第三方包，已开始使用go module，但没有任何外部依赖")
 }
