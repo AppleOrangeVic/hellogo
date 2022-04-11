166e147e7867ad44ab71194c955dbcd9f6d519d1
boyvic <117716643@qq.com>
2022-04-11 09:25:09 +0000

    add module

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/boyvic/hellogo
 
 go 1.14
+
+require github.com/google/uuid v1.3.0
--- main.go
+++ main.go
@@ -1,11 +1,21 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+
+	"github.com/google/uuid"
+)
 
 func main() {
 	// v1.0.0 未引用任何第三方包，也未使用go module
 	// fmt.Println("v1.0.0 未引用任何第三方包，也未使用go module")
 
 	// v1.1.0 未引用任何第三方包，已开始使用go module，但没有任何外部依赖
-	fmt.Println("v1.1.0 未引用任何第三方包，已开始使用go module，但没有任何外部依赖")
+	// fmt.Println("v1.1.0 未引用任何第三方包，已开始使用go module，但没有任何外部依赖")
+
+	// v1.2.0 引用了第三方包，并更新了项目依赖
+	fmt.Println("v1.2.0 引用了第三方包，并更新了项目依赖")
+
+	id := uuid.New().String()
+	fmt.Println("UUID: ", id)
 }
